beef9b814fb38a6ab7ae1c6fe7df2bd039011900
gabrielsousa99-dev <gabrielbojes360@gmail.com>
2026-04-21 22:19:41 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    //if(html.classList.contains('light')) {
+        //html.classList.remove('light')
+    //} else {
+       // html.classList.add('light')
+    //}
+
+    html.classList.toggle('light')
+}
+
+
+
+//const img = document.queryselector("#profile img")
+//if(html.classList.contain('ligth')){
+//img.setAttribute('src', './)
+//} else {
+//img.setAttribute("src", "./")
+//}    }
+
